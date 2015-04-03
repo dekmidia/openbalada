b4a41bee008d673db8e0193f7fd3322f3d5a4b34
Cristiano Kilê Pereira <criskile@gmail.com>
2015-04-02 23:14:35 -0300

    OpenBalada-2015-04-02-2314
    Atualização de arquivos

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,14 @@
 <?php
+
+
+
+/** Enable W3 Total Cache Edge Mode */
+
+define('W3TC_EDGE_MODE', true); // Added by W3 Total Cache
+
+
+define( 'WP_MEMORY_LIMIT', '256M' );
+
 /** 
  * As configurações básicas do WordPress.
  *
@@ -19,15 +29,16 @@
 define('DB_NAME', 'openbala_obdb_v1');
 
 /** Usuário do banco de dados MySQL */
-//define('DB_USER', 'openbala_opnblda');
 define('DB_USER', 'root');
+/*define('DB_USER', 'openbala_opnblda');*/
 
-/** Senha do banco de dados MySQL **/
-//define('DB_PASSWORD', 's@&5IH4u');
+/** Senha do banco de dados MySQL */
 define('DB_PASSWORD', '');
+/*define('DB_PASSWORD', 's@&5IH4u');*/
 
 /** nome do host do MySQL */
 define('DB_HOST', 'localhost');
+
 
 /** Conjunto de caracteres do banco de dados a ser usado na criação das tabelas. */
 define('DB_CHARSET', 'utf8');
@@ -45,13 +56,21 @@ define('DB_COLLATE', '');
  * @since 2.6.0
  */
 define('AUTH_KEY',         ']=o?f!p|SU&$pDO;.;OVG+_Vb}qwj<N39a:s;B@[/<6p$|BW|U/+:[dmkHn:71-E');
+
 define('SECURE_AUTH_KEY',  '7K8SvbD#Jvfu&dUL,OW5]$RwC4~E-[4f76M,3QGfB-gmtR4PAV:|3X]JU1vr [1$');
+
 define('LOGGED_IN_KEY',    'hcv-nirX9r|u`&jCN.9u|i,.+!1dv<g~9 Oz;zanB>eYuE ,X:+^rvW.H!G+3kw[');
+
 define('NONCE_KEY',        '-wZXF/[aN7XM-8i.zDh(KYF -a68ZIZY|Q8C8HqlfwZW|NxU:%h&u@},.g6u0kP+');
+
 define('AUTH_SALT',        '>|6b.VXX<g|8%&ii$$f9C}SfU=7]u0*18mp-94=uA3v2=&&Oi6MNZk7-%E{0} qY');
+
 define('SECURE_AUTH_SALT', 'CdIGW9PRCbptU-E:h!@[T{aUixpH4|nS.H&ZIlqq4`0g9xU]_Dra1>_&}3-f?i??');
+
 define('LOGGED_IN_SALT',   'le&Ot)ovS7; !ZA(d)N=vI]bE~$]Z H_=1~-yr2Pa4V-W-|>xlYwLeTu c$_p*Z@');
+
 define('NONCE_SALT',       'wP,+yr%:`ac=T2`cb(#6`4,Z{7{]y+Ni Nv)eixhRUtBNfPXbIt)7L9R1XeY20gM');
+
 
 /**#@-*/
 
@@ -62,6 +81,7 @@ define('NONCE_SALT',       'wP,+yr%:`ac=T2`cb(#6`4,Z{7{]y+Ni Nv)eixhRUtBNfPXbIt)
  * prefixo. Somente números, letras e sublinhados!
  */
 $table_prefix  = 'wp_';
+
 
 
 /**
